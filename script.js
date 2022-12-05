e5f6faec36e97acadfda9682c2b8460071c9e193
Pawel <100613143+RosinskiPawel@users.noreply.github.com>
2022-12-05 22:19:12 +0100

    finall (?) mobile version

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 const autor = document.querySelector('.author')
-const arrow = document.querySelector('.arrow-share')
+const newAutor = document.querySelector('.author_after-click')
+const arrow = document.getElementById('first-arrow')
 const pic = autor.querySelector('img')
 const opis = document.querySelector('.description')
+// const newButton = document.querySelector('.author_after-click')
+const newArrow = document.getElementById('grey-arrow')
 
-arrow.addEventListener('mouseover', function () {
-  autor.style.backgroundColor = 'hsl(217, 19%, 35%)'
-  opis.style.backgroundColor = 'green'
-  pic.style.display = 'none'
+autor.addEventListener('click', function () {
+  newAutor.style.display = 'block'
 })
-arrow.addEventListener('mouseout', function () {
-  autor.style.backgroundColor = ''
-  opis.style.backgroundColor = ''
-  pic.style.display = ''
+newAutor.addEventListener('click', function () {
+  newAutor.style.display = ''
 })
+
+// opis.addEventListener('mouseover', function () {
+//   autor.style.backgroundColor = 'hsl(217, 19%, 35%)'
+//   opis.style.backgroundColor = 'green'
+//   pic.style.display = 'none'
+// })
+// opis.addEventListener('mouseout', function () {
+//   autor.style.backgroundColor = ''
+//   opis.style.backgroundColor = ''
+//   pic.style.display = ''
+// })
+
+// arrow.addEventListener('mouseover', function () {
+//   newButton.style.display = 'block'
+// })
+// arrow.addEventListener('mouseout', function () {
+//   newButton.style.display = ''
+// })
