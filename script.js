9397070a85c9380cf4d5dc059ef667e78f550e6e
Pawel <100613143+RosinskiPawel@users.noreply.github.com>
2022-12-07 22:41:58 +0100

    BEM methodology, cleaned JS

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-const autor = document.querySelector('.author')
-const newAutor = document.querySelector('.author_after-click')
-const arrow = document.getElementById('first-arrow')
-const pic = autor.querySelector('img')
-const opis = document.querySelector('.description')
-// const newButton = document.querySelector('.author_after-click')
-const newArrow = document.getElementById('grey-arrow')
+const autor = document.querySelector('.author-before')
+const newAutor = document.querySelector('.author-after')
 
 autor.addEventListener('click', function () {
   newAutor.style.display = 'block'
@@ -12,21 +7,3 @@ autor.addEventListener('click', function () {
 newAutor.addEventListener('click', function () {
   newAutor.style.display = ''
 })
-
-// opis.addEventListener('mouseover', function () {
-//   autor.style.backgroundColor = 'hsl(217, 19%, 35%)'
-//   opis.style.backgroundColor = 'green'
-//   pic.style.display = 'none'
-// })
-// opis.addEventListener('mouseout', function () {
-//   autor.style.backgroundColor = ''
-//   opis.style.backgroundColor = ''
-//   pic.style.display = ''
-// })
-
-// arrow.addEventListener('mouseover', function () {
-//   newButton.style.display = 'block'
-// })
-// arrow.addEventListener('mouseout', function () {
-//   newButton.style.display = ''
-// })
