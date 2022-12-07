c01091ee10fd5382a268e09d8198f44f4f43fdac
Pawel <100613143+RosinskiPawel@users.noreply.github.com>
2022-12-08 00:10:05 +0100

    new

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 const autor = document.querySelector('.author-before')
 const newAutor = document.querySelector('.author-after')
 
-autor.addEventListener('click', function () {
+// autor.addEventListener('click', function () {
+//   newAutor.style.display = 'block'
+// })
+// newAutor.addEventListener('click', function () {
+//   newAutor.style.display = ''
+// })
+
+// const autor = document.querySelector('.author-before')
+
+// function greyshare() {
+//   autor.classList.toggle('author-after')
+// }
+const clickArrow = autor.querySelector('.author-before__arrow')
+const clickNewArrow = newAutor.querySelector('.author-after__arrow')
+
+clickArrow.addEventListener('click', function () {
   newAutor.style.display = 'block'
 })
-newAutor.addEventListener('click', function () {
+clickNewArrow.addEventListener('click', function () {
   newAutor.style.display = ''
 })
