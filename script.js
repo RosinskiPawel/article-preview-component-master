6667a8620e6782402d469884af4eca0d4a6f942a
Pawel <100613143+RosinskiPawel@users.noreply.github.com>
2022-12-10 19:40:33 +0100

    new JS

--- script.js
+++ script.js
@@ -15,10 +15,20 @@ const newAutor = document.querySelector('.author-after')
 // }
 const clickArrow = autor.querySelector('.author-before__arrow')
 const clickNewArrow = newAutor.querySelector('.author-after__arrow')
+// const text = document.querySelector('.header-text')
+// function changeColor() {
+//   text.style.color = 'red'
+// }
 
-clickArrow.addEventListener('click', function () {
+// clickArrow.addEventListener('click', function () {
+//   newAutor.style.display = 'block'
+// })
+// clickNewArrow.addEventListener('click', function () {
+//   newAutor.style.display = ''
+// })
+function showShare() {
   newAutor.style.display = 'block'
-})
-clickNewArrow.addEventListener('click', function () {
+}
+function hideShare() {
   newAutor.style.display = ''
-})
+}
